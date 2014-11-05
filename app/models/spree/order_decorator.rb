81bdb7340c0fc0e0e04be94c3babdd858494b445
Wooobee <m.laux@wooobee.com>
2014-11-05 10:42:02 +0100

    flow updated

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,9 +1,31 @@
 Spree::Order.class_eval do
 
   # attr_accessible :terms_and_conditions # uncomment for Spree below version 2.1
+  
+  checkout_flow do
+    go_to_state :address
+    go_to_state :delivery
+    go_to_state :payment, if: ->(order) {
+    order.update_totals
+    order.payment_required?
+    }
+    go_to_state :terms_and_conditions
+    go_to_state :confirm, if: ->(order) { order.confirmation_required? }
+    go_to_state :complete
+  end
+
+  # If true, causes the payment step to happen during the checkout process
+  def payment_required?
+    return true
+  end
+
+  # If true, causes the confirmation step to happen during the checkout process
+  def confirmation_required?
+    return true
+  end
 
   # Add new checkout step to checkout process
-  insert_checkout_step :terms_and_conditions, :before => :confirm
+  #insert_checkout_step :terms_and_conditions, :before => :confirm
 
   def valid_terms_and_conditions?
     logger.debug "Terms and Conditions: #{terms_and_conditions}"
@@ -17,10 +39,6 @@ Spree::Order.class_eval do
   end
 end
 
- # If true, causes the confirmation step to happen during the checkout process
-  def confirmation_required?
-    return true
-  end
 
 # Validate on state change
 Spree::Order.state_machine.before_transition :to => :confirm, :do => :valid_terms_and_conditions?
